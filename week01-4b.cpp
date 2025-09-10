3574ec9e5fa27c9f031541d76bd88b56303bb0ae
624liang <13750280@me.mcu.edu.tw>
2025-09-10 11:57:22 +0800

    week01

--- week01-4b.cpp
+++ week01-4b.cpp
@@ -1,13 +1,13 @@
-/// week01-4b.cpp 使用 C++ 語言寫
-#include <iostream> ///使用 C語言 外掛
+/// week01-4b.cpp 使用 C++ 寫
+#include <iostream> ///使用 C++ 外掛
 using namespace std;
 int main()
 {
     int a,b;
-    cin >> a >> b; /// C語言 讀資料
+    cin >> a >> b; /// C++ 讀資料
     int ans = 0;
     for(int i=a; i<=b; i++){
         if( i%3==0 ) ans += i;
     }
-    cout <<ans; /// C語言 印資料
+    cout <<ans; /// C++ 印資料
 }
